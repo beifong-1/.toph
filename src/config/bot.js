427b27f6614325e6b27c642daa4e77a07105d3da
beifong-1 <scottkeyvinski@gmail.com>
2026-05-13 18:16:29 +0100

    Update bot.js

--- src/config/bot.js
+++ src/config/bot.js
@@ -25,7 +25,7 @@ export const botConfig = {
     activities: [
       {
         // Text users will see (example: "Playing /help | Titan Bot").
-        name: "watching gg.toph",
+        name: "watching over ノ𝙄𝘾𝙀𝙀",
         // Activity type number (0 = Playing).
         type: 0, 
       },
